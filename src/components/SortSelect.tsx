80e1a3527128cf94f7a6b11fb5a92ee60f80f503
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-03-14 10:04:25 +0900

    検索機能のリプレイス

--- src/components/SortSelect.tsx
+++ src/components/SortSelect.tsx
@@ -1,18 +1,37 @@
-import styles from 'styles/search.module.css'
+import styles from 'styles/search.module.css';
 
-export default function SortSelect ({onSortChange}: {onSortChange: (value: string)=>void}) {
-    const sortChange = (value: string) => {
-        onSortChange(value);
-    }
-    return(
-        <>
-        <form action="GET" >
-        <select  className={styles.sortSelect} onChange={(e)=>sortChange(e.target.value)}>
-            <option className={styles.sortSelect} value='itemId,desc'>新着順</option>
-            <option className={styles.sortSelect} value='twoDaysPrice,desc'>価格が高い順</option>
-            <option className={styles.sortSelect} value='twoDaysPrice,asc'>価格が安い順</option>
+export default function SortSelect({
+  onSortChange,
+}: {
+  onSortChange: (value: string) => void;
+}) {
+  const sortChange = (value: string) => {
+    onSortChange(value);
+  };
+  return (
+    <>
+      <form action="GET">
+        <select
+          className={styles.sortSelect}
+          onChange={(e) => sortChange(e.target.value)}
+        >
+          <option className={styles.sortSelect} value="id,desc">
+            新着順
+          </option>
+          <option
+            className={styles.sortSelect}
+            value="twoDaysPrice,desc"
+          >
+            価格が高い順
+          </option>
+          <option
+            className={styles.sortSelect}
+            value="twoDaysPrice,asc"
+          >
+            価格が安い順
+          </option>
         </select>
-        </form>
-        </>
-    )
+      </form>
+    </>
+  );
 }
